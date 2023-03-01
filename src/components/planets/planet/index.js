cfe85a06381f3a36b1534a964609635afb6375ba
Rafael Damasceno <rafaelsd389@gmail.com>
2023-02-28 21:52:12 -0300

    Atualização do projeto React - Aprendendo State

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -3,12 +3,6 @@ import DescriptionWithLink from "../shared/description_with_link";
 import GrayImg from "../shared/gray_img";
 
 function Planet(props) {
-    /* const names = ['Lua', 'MySQL', 'Java', 'JS']
-    const satelites = names.map((elemento) =>
-        <li>Satélite {elemento}</li>
-    )*/
-
-
     let title;
     if (props.titleUnderline) {
         title = <h4><u>{props.nome}</u></h4>
@@ -21,14 +15,7 @@ function Planet(props) {
             {title}
             <DescriptionWithLink description={props.description} link_url={props.link_url} />
             <GrayImg img_url={props.img_url} gray={props.gray} />
-
-            <h4>Satélites: </h4>
-            <ul>
-                {['Lua', 'MySQL', 'Java', 'JS'].map((elemento) =>
-                    <li>Satélite {elemento}</li>
-                )}
-            </ul>
-
+            <hr />
         </div>
     )
 }
